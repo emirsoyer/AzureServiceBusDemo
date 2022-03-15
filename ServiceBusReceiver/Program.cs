4d47dff6a724f151d0789ea9ebda33d5f45c63b7
emirsoyer <emir.afk@gmail.com>
2022-03-15 11:16:48 +0300

    Add project files.

---
+++ ServiceBusReceiver/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.ServiceBus;
+using ServiceBusShared.Models;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusReceiver
+{
+    class Program
+    {
+
+        const string connectionString= "Endpoint=sb://asoyerservicebusdemo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=YqhhXeDx0m6+EaI58mDarbO6c5vsdcFDBJQJGdhOPuA=";
+        const string queueName = "personqueue";
+        static IQueueClient queueClient;
+
+        static async Task Main(string[] args)
+        {
+            queueClient = new QueueClient(connectionString, queueName);
+
+            var messageHandlerOptions = new MessageHandlerOptions(exceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+
+            Console.ReadLine();
+
+            await queueClient.CloseAsync();
+        }
+
+        private static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            var jsonString = Encoding.UTF8.GetString(message.Body);
+            PersonModel person = JsonSerializer.Deserialize<PersonModel>(jsonString);
+            Console.WriteLine($"Form received from {person.FirstName} {person.LastName}");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private static Task exceptionReceivedHandler(ExceptionReceivedEventArgs arg)
+        {
+            Console.WriteLine($"Message exception handler {arg.Exception}");
+            return Task.CompletedTask;
+        }
+    }
+}
